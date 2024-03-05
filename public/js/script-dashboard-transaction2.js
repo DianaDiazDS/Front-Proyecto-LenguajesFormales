111a4747e4c69b55bed8b82696c6022512a2acc4
Juanita Sanabria <93340989+JuanitaSnz@users.noreply.github.com>
2024-03-05 15:04:36 -0500

    regex

--- public/js/script-dashboard-transaction2.js
+++ public/js/script-dashboard-transaction2.js
@@ -1,6 +1,7 @@
 const apiUrl = "http://localhost:4000/transaction";
 // const authorizationToken = localStorage.getItem("login");
 const authorizationToken = localStorage.getItem("login");
+const iduser = localStorage.getItem("iduser");
 
 // const isAuthenticated = () => {
 //   return authorizationToken !== null && authorizationToken !== undefined;
@@ -47,13 +48,6 @@ const loadTable = () => {
   optionDefault.innerText = "Seleccione una categoria";
   document.getElementById("select-id").appendChild(optionDefault);
 
-
-  document.getElementById("select-id2").innerHTML = "";
-  const optionDefault2 = document.createElement("option");
-  optionDefault2.value = "Seleccione  estado";
-  optionDefault2.innerText = "Seleccione estado";
-  document.getElementById("select-id2").appendChild(optionDefault2);
-
   const clientId = localStorage.getItem("iduser");
   if (!clientId) {
     console.error("No se encontró el ID del cliente en el localStorage");
@@ -70,8 +64,6 @@ const loadTable = () => {
       .then((datos) => datos.json())
       .then((datos) => {
         const select = document.getElementById("select-id");
-        const select2 = document.getElementById("select-id2");
-
 
 
 
@@ -172,11 +164,6 @@ const loadTable = () => {
             option.innerText = transaccion.category;
             select.appendChild(option);
 
-            const option2 = document.createElement("option");
-            option2.value = transaccion.status;
-            option2.innerText = transaccion.status;
-            select2.appendChild(option2);
-
             document.getElementById("table-body").appendChild(row);
           }
         });
@@ -195,132 +182,6 @@ const findByCategoria = () => {
     return new Promise((resolve, reject) => {
       fetch(
         `http://localhost:4000/transaction/categoria/${option.value}`,
-        {
-          headers: {
-            Authorization: `${authorizationToken}`,
-          },
-        }
-      )
-        .then((data) => {
-          if (!data.ok) {
-            throw new Error(`Error: ${data.status} - ${data.statusText}`);
-          }
-          return data.json();
-        })
-        .then((result) => {
-          document.getElementById("table-body").innerHTML = "";
-
-
-          const clientId = localStorage.getItem("iduser");
-          if (!clientId) {
-            console.error("No se encontró el ID del cliente en el localStorage");
-            return;
-          }
-
-          result.data.forEach((transaccion) => {
-            const paymentDate = formatDate(transaccion.paymentDate);
-            const endDate = formatDate(transaccion.endDate);
-
-            const row = document.createElement("tr");
-            row.innerHTML = `
-          <td>${transaccion.id}</td>
-          <td>${transaccion.amount}</td>
-          <td>${transaccion.status}</td>
-          <td>${transaccion.entityname}</td>
-          <td>${paymentDate}</td>
-          <td>${endDate}</td>
-          <td>${transaccion.category}</td>
-        
-                <td>      
-                
-                    <i class="bi bi-pencil-fill"
-                    type="button" 
-                    data-bs-toggle="modal"
-                    data-bs-target="#editModal${transaccion._id}" 
-                    style="color: #FFC300; font-size: 2rem;">
-                    </i>
-                    <!-- Modal -->
-                    <div class="modal fade" id="editModal${transaccion._id
-              }" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="editModalLabel" aria-hidden="true">
-                        <div class="modal-dialog">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <h1 class="modal-title fs-5" id="editModalLabel">Editar transaccion</h1>
-                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                </div>
-                                <div class="modal-body">            
-                
-                                    <div class="mb-3">
-                                        <label for="update-amount" class="form-label">cantidad</label>
-                                        <input type="text" class="form-control" id="update-amount${transaccion._id
-              }" value="${transaccion.amount}">
-                                    </div>
-                                    <div class="mb-3">
-                                        <label for="update-status" class="form-label">estado</label>
-                                        <input type="tel" class="form-control" id="update-status${transaccion._id
-              }" value="${transaccion.status}">
-                                    </div>
-                                    <div class="mb-3">
-                                        <label for="update-entityname" class="form-label">entityname</label>
-                                        <input type="tel" class="form-control" id="update-entityname${transaccion._id
-              }" value="${transaccion.entityname}">
-                                    </div>
-                                    <div class="mb-3">
-                                        <label for="update-paymentDate" class="form-label">fecha pago</label>
-                                        <input type="text" class="form-control" id="update-paymentDate${transaccion._id
-              }" value="${paymentDate}">
-                                    </div>
-                                    <div class="mb-3">
-                                        <label for="update-endDate" class="form-label">fecha limite</label>
-                                        <input type="text" class="form-control" id="update-endDate${transaccion._id
-              }" value="${endDate}">
-                                    </div>
-                                    <div class="mb-3">
-                                    <label for="update-category" class="form-label">categoria</label>
-                                    <input type="text" class="form-control" id="update-category${transaccion._id
-              }" value="${transaccion.category}">
-                                     </div>
-                                    
-                                    
-                                    
-                                </div>
-
-
-                                <div class="modal-footer">                     
-                                    <button type="button" class="btn btn-secondary" onclick="loadTable()" data-bs-dismiss="modal">Cerrar</button>
-                                <button type="button" class="btn btn-primary" onclick="updateTransaction('${transaccion.id}','${transaccion._id}' )">Actualizar transaccion</button>
-                                </div>
-                            </div>
-                        </div>6
-                    </div>
-            
-
-                </td> 
-
-                <td>
-                <i class="bi bi-x-circle" data-value='${transaccion.id}' type="button" onclick='drop(this.getAttribute("data-value"))' style="color: red; font-size: 2rem;"></i>
-                </td>
-
-                    
-            `;
-
-
-
-
-            document.getElementById("table-body").appendChild(row);
-          });
-        })
-        .catch((error) => reject(error));
-    });
-  }
-};
-
-const findByStatus = () => {
-  const option = document.getElementById("select-id2");
-  if (option.value !== "Seleccione una estado") {
-    return new Promise((resolve, reject) => {
-      fetch(
-        `http://localhost:4000/transaction/status/${option.value}`,
         {
           headers: {
             Authorization: `${authorizationToken}`,
@@ -539,81 +400,7 @@ function convertirFecha(fecha) {
 function quitarComas(numeroConComas) {
   return numeroConComas.replace(/,/g, "");
 }
-const addRellenarTransaction=()=>{
-  const mensajeTexto = document.getElementById("mensajeTexto").value;
 
-    
-    // const amountRegex = /Valor:\s*([\d.,]+)/i;
-    // const entitynameRegex = /factura\s+(\w+)\s+postpago/i;
-    // const paymentDateRegex = /(\d{2}\/\d{2}\/\d{4})/; // Formato: dd/mm/yyyy
-    // const endDateRegex = /Fecha\s+limite\s+de\s+pago:\s+(\d{2}\/\w+\/\d{4})/i; 
-    // const categoryRegex = /(\w+)\s+postpago\s+del\s+mes\s+de/i;
-    // const statusRegex = /gracias\s+por\s+tu\s+pago/i;
-        
-    const amountRegex = /(\$?\d{1,3}(?:,\d{3})*(?:\.\d{2})?)/;
-    const paymentDateRegex = /(\d{2}\/(?:\d{2}|[a-zA-Z]{3})\/\d{4})/;
-    // const entitynameRegex = /factura\s+(\w+)\s+postpago/i;
-    // const paymentDateRegex = /(\d{2}\/\d{2}\/\d{4})/; // Formato: dd/mm/yyyy
-    // const endDateRegex = /Fecha\s+limite\s+de\s+pago:\s+(\d{2}\/\d+\/\d{4})/i; 
-    // const categoryRegex = /(\w+)\s+postpago\s+del\s+mes\s+de/i;
-    // const statusRegex = /gracias\s+por\s+tu\s+pago/i;
-   
-    console.log(amountRegex)
-
-    const amountMatch = mensajeTexto.match(amountRegex);
-    // const entitynameMatch = mensajeTexto.match(entitynameRegex);
-    const paymentDateMatch = mensajeTexto.match(paymentDateRegex);
-    // const endDateMatch = mensajeTexto.match(endDateRegex);
-    // const categoryMatch = mensajeTexto.match(categoryRegex);
-    // const statusMatch = mensajeTexto.match(statusRegex);
-
-
-    // _para modificar la fehcha
-    // console.log("fecha",endDateMatch[1])
-    // const fechaFormateada = convertirFecha(endDateMatch[1]);
-    console.log("fecha",paymentDateMatch[1])
-    const fechaFormateada = convertirFecha(paymentDateMatch[1]);
-
-    // const numeroConComas =amountMatch[1];
-    // const numeroSinComas = quitarComas(numeroConComas);
-
-console.log("fechaaaa",fechaFormateada)
-
-    // if (!amountMatch || !entitynameMatch || !paymentDateMatch || !endDateMatch || !categoryMatch) {
-    //     console.log("No se pudo extraer la información completa de la transacción.");
-        
-    //      }  
-
-    const amount = amountMatch ? amountMatch[1] : "";
-
-    // const amount = numeroSinComas ? numeroSinComas : "";
-    // const entityname = entitynameMatch ? entitynameMatch[1] : "";
-    // const paymentDate = paymentDateMatch ? paymentDateMatch[1] : "";
-    const endDate = fechaFormateada ? fechaFormateada : "";
-    // const category = categoryMatch ? categoryMatch[1] : "";
-    // const status = statusMatch ? "pago" : "noPago";
-
-
-    document.getElementById("amount").value = amount;
-    // document.getElementById("entityname").value = entityname;
-    // document.getElementById("paymentDate").value = paymentDate;
-    // document.getElementById("endDate").value = endDate;
-    // document.getElementById("category").value = category;
-    // document.getElementById("status").value = status;
-
-    console.log("amount",amount);
-    // console.log("entityname",entityname);
-    // console.log("paymentDate",paymentDate);
-    // console.log("endDate",endDate);
-    // console.log("category",category);
-    // console.log(" status", status);
-
-    
-
-
-
-    console.log("¡Transacción agregada con éxito!");
-}
 
 const addTransaction = () => {
   const id = document.getElementById("id").value;
